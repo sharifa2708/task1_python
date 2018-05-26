466681ccd0023b443adf17c2f2e3ed8512452628
dps1 <sanjay5803@gmail.com>
2018-05-26 15:44:44 +0000

    task1_python

---
+++ task_1.1.py
@@ -0,0 +1,34 @@
+#To compare two text files
+
+#creating list containing the sentences of each file
+f1 = open("file1.txt").readlines()
+f2 = open('file2.txt','r').readlines()
+
+
+
+#printing the difference in the 2 files.
+
+#loop cheching if the elements of same index is same
+#if not then print both the elements
+
+print("Difference in statement of God and Follower:")
+for i in range(0, len(f1)):
+    if f1[i]==f2[i]:
+        i=i+1
+        pass
+    else:
+        print('-' + f1[i])
+        print('+' + f2[i])
+        i=i+1
+        continue
+
+
+
+     
+
+  
+
+
+
+
+
